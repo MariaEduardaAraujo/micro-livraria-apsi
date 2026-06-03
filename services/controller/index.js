c1f2546cdc2535a4357aee6b34c6a1d46cfcf1a1
MariaEduardaAraujo <mariaeduardadearaujosilva@outlook.com>
2026-06-03 10:25:31 -0300

    feat: adicionando calculo de desconto ao envio

--- services/controller/index.js
+++ services/controller/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const shipping = require('./shipping');
 const inventory = require('./inventory');
+const discount = require('./discount');
 const cors = require('cors');
 
 const app = express();
@@ -21,25 +22,27 @@ app.get('/products', (req, res, next) => {
 });
 
 /**
- * Consulta o frete de envio no ShippingService
+ * Consulta o frete de envio no ShippingService e calcula desconto no frete
  */
 app.get('/shipping/:cep', (req, res, next) => {
-    shipping.GetShippingRate(
-        {
-            cep: req.params.cep,
-        },
-        (err, data) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send({ error: 'something failed :(' });
-            } else {
-                res.json({
-                    cep: req.params.cep,
-                    value: data.value,
-                });
-            }
+    shipping.GetShippingRate({ cep: req.params.cep }, (err, shippingData) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send({ error: 'something failed :(' });
         }
-    );
+        discount.GetDiscountRate({}, (err, discountData) => {
+            if (err){
+                console.error(err)
+                return res.status(500).send({ error: 'something failed :('})
+            }
+            res.json({
+                cep: req.params.cep,
+                shipping: shippingData.value,
+                discount: discountData.value,
+                total: shippingData.value - discountData.value,
+            });
+        });
+    });
 });
 
 /**
